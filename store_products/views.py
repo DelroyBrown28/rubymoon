11a077f46b7609318dfda3caa60d2005481c747f
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-05 14:57:45 +0100

    store products showing in page!

--- store_products/views.py
+++ store_products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    # Shows all products
+
+    store_products = Product.objects.all()
+
+    context = {
+        'store_products': store_products
+    }
+    return render(request, 'products/products.html', context)
