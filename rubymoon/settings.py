794a5e1647683b73fc5a92975ed9279f3856600d
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-05 14:15:40 +0100

    added store products and categories through json loaddata

--- rubymoon/settings.py
+++ rubymoon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'store_products',
 ]
 
 MIDDLEWARE = [
@@ -142,9 +143,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files ')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
