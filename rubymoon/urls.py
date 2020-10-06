abc53993f00a8bfb1cbb956fab8b2108b114300d
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-06 22:43:07 +0100

    can now view items on product_inf page

--- rubymoon/urls.py
+++ rubymoon/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from store_products import views
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('store_products/', include('store_products.urls')),
+    path('<int:product_id>', views.product_info, name='product_info'),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
